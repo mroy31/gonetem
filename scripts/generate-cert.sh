d8d028af818fa369fee3cd3cb71983de50992801
Mickael Royer <mickael.royer@enac.fr>
2024-01-03 17:09:20 +0100

    [tls/ssl] up date script to generate console/server certificates

--- scripts/generate-cert.sh
+++ scripts/generate-cert.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-rm *.pem
+DURATION=3650
+rm *.pem *.srl
 
 # 1. Generate CA's private key and self-signed certificate
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem \
+openssl req -x509 -newkey rsa:4096 -days ${DURATION} -nodes -keyout ca-key.pem -out ca-cert.pem \
     -subj "/C=FR/ST=Occitanie/L=Toulouse/O=Tech School/OU=Education/CN=*.rca.enac.fr/emailAddress=rca@enac.fr"
 
 echo "CA's self-signed certificate"
@@ -14,7 +15,7 @@ openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem \
     -subj "/C=FR/ST=Occitanie/L=Toulouse/O=Tech School/OU=Education/CN=*gonetem-server.rca.enac.fr/emailAddress=rca@enac.fr"
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
-openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem \
+openssl x509 -req -in server-req.pem -days ${DURATION} -CA ca-cert.pem -CAkey ca-key.pem \
     -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
 echo "Server's signed certificate"
@@ -25,8 +26,10 @@ openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem \
     -subj "/C=FR/ST=Occitanie/L=Toulouse/O=Tech School/OU=Education/CN=*gonetem-client.rca.enac.fr/emailAddress=rca@enac.fr"
 
 # 5. Use CA's private key to sign client's CSR and get back the signed certificate
-openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem \
+openssl x509 -req -in client-req.pem -days ${DURATION} -CA ca-cert.pem -CAkey ca-key.pem \
     -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
 
 echo "Client's signed certificate"
 openssl x509 -in client-cert.pem -noout -text
+
+rm ca-key.pem ca-cert.srl
